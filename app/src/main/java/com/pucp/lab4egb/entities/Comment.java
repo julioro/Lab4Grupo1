c6b8c54b3ce56c74e935489dc19ca69d77db2e34
Gabriel Gamero <gabriel.gamero@pucp.pe>
2020-07-05 22:13:20 -0500

    Entidades Photo y Comment creadas

---
+++ app/src/main/java/com/pucp/lab4egb/entities/Comment.java
@@ -0,0 +1,41 @@
+package com.pucp.lab4egb.entities;
+
+public class Comment {
+
+    private String body;
+    private String user;
+    private String date;
+    private String hour;
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getHour() {
+        return hour;
+    }
+
+    public void setHour(String hour) {
+        this.hour = hour;
+    }
+}
---
+++ app/src/main/java/com/pucp/lab4egb/entities/Photo.java
@@ -0,0 +1,52 @@
+package com.pucp.lab4egb.entities;
+
+import java.util.ArrayList;
+
+public class Photo {
+
+    private String user;
+    private String image;
+    private String date;
+    private String description;
+    private ArrayList<Comment> comments;
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public ArrayList<Comment> getComments() {
+        return comments;
+    }
+
+    public void setComments(ArrayList<Comment> comments) {
+        this.comments = comments;
+    }
+}
