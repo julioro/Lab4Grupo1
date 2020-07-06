643014c48e10d15993504c6573ea3cb8f427bc54
Eduardo <eduardo.requena@pucp.pe>
2020-07-06 14:41:21 -0500

    Prueba1 Twitter

---
+++ app/src/main/java/com/pucp/lab4egb/LoginActivity.java
@@ -0,0 +1,170 @@
+package com.pucp.lab4egb;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GithubAuthProvider;
+import com.google.firebase.auth.TwitterAuthProvider;
+import com.twitter.sdk.android.core.Callback;
+import com.twitter.sdk.android.core.Result;
+import com.twitter.sdk.android.core.Twitter;
+import com.twitter.sdk.android.core.TwitterAuthConfig;
+import com.twitter.sdk.android.core.TwitterConfig;
+import com.twitter.sdk.android.core.TwitterCore;
+import com.twitter.sdk.android.core.TwitterException;
+import com.twitter.sdk.android.core.TwitterSession;
+import com.twitter.sdk.android.core.identity.TwitterLoginButton;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.WindowManager;
+import android.webkit.WebView;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import java.io.IOException;
+import java.math.BigInteger;
+import java.security.SecureRandom;
+
+
+
+
+public class LoginActivity extends AppCompatActivity {
+
+
+
+    private FirebaseAuth firebaseAuth;
+    private FirebaseAuth.AuthStateListener authStateListener;
+    private TwitterLoginButton mTwitterBtn;
+    private ProgressBar mIndeterminateProgressBar;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+
+
+
+        TwitterAuthConfig mTwitterAuthConfig = new TwitterAuthConfig(getString(R.string.twitter_consumer_key),
+                getString(R.string.twitter_consumer_secret));
+        TwitterConfig twitterConfig = new TwitterConfig.Builder(this)
+                .twitterAuthConfig(mTwitterAuthConfig)
+                .build();
+        Twitter.initialize(twitterConfig);
+        setContentView(R.layout.activity_login);
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        mTwitterBtn = findViewById(R.id.twitter_login_button);
+        mIndeterminateProgressBar = findViewById(R.id.indeterminateProgressBar);
+
+        authStateListener = new FirebaseAuth.AuthStateListener(){
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth){
+                if (firebaseAuth.getCurrentUser() != null){
+                    startActivity(new Intent(LoginActivity.this, MainActivity.class));
+                }
+            }
+        };
+
+        UpdateTwitterButton();
+
+        mTwitterBtn.setCallback(new Callback<TwitterSession>() {
+
+            @Override
+            public void success(Result<TwitterSession> result) {
+                Toast.makeText(LoginActivity.this, "Signed in to twitter successful", Toast.LENGTH_LONG).show();
+                signInToFirebaseWithTwitterSession(result.data);
+                mTwitterBtn.setVisibility(View.VISIBLE);
+                mIndeterminateProgressBar.setVisibility(View.VISIBLE);
+                getWindow().setFlags(WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE, WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE);
+            }
+
+            @Override
+            public void failure(TwitterException exception) {
+                Toast.makeText(LoginActivity.this, "Login failed. No internet or No Twitter app found on your phone", Toast.LENGTH_LONG).show();
+                getWindow().clearFlags(WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE);
+                mIndeterminateProgressBar.setVisibility(View.GONE);
+                UpdateTwitterButton();
+            }
+        });
+
+
+
+        }
+
+
+
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        mTwitterBtn.onActivityResult(requestCode, resultCode, data);
+
+    }
+    @Override
+    public void onStart() {
+        super.onStart();
+        FirebaseUser currentUser = firebaseAuth.getCurrentUser();
+        if (currentUser != null) {
+            updateUI();
+        }
+        firebaseAuth.addAuthStateListener(authStateListener);
+    }
+
+    private void updateUI() {
+        Toast.makeText(LoginActivity.this, "You're logged in", Toast.LENGTH_LONG);
+        //Sending user to new screen after successful login
+        Intent mainActivity = new Intent(LoginActivity.this, LoginActivity.class);
+        startActivity(mainActivity);
+        finish();
+    }
+
+    @Override
+    protected void onDestroy(){
+        super.onDestroy();
+        firebaseAuth.removeAuthStateListener(authStateListener);
+    }
+    private void UpdateTwitterButton(){
+        if (TwitterCore.getInstance().getSessionManager().getActiveSession() == null){
+          // mTwitterBtn.setVisibility(View.INVISIBLE);
+        }
+        else{
+            mTwitterBtn.setVisibility(View.GONE);
+        }
+    }
+
+
+    private void signInToFirebaseWithTwitterSession(TwitterSession session){
+        AuthCredential credential = TwitterAuthProvider.getCredential(session.getAuthToken().token,
+                session.getAuthToken().secret);
+
+        firebaseAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        Toast.makeText(LoginActivity.this, "Signed in firebase twitter successful", Toast.LENGTH_LONG).show();
+                        if (!task.isSuccessful()){
+                            Toast.makeText(LoginActivity.this, "Auth firebase twitter failed", Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
+    }
+
+
+
+    }
+
+
+
--- app/src/main/java/com/pucp/lab4egb/MainActivity.java
+++ app/src/main/java/com/pucp/lab4egb/MainActivity.java
@@ -5,35 +5,39 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.TextView;
 
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import org.w3c.dom.Text;
+
 public class MainActivity extends AppCompatActivity {
     DatabaseReference mDatabase;
+    FirebaseAuth mAuth;
+    String nombre="";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mAuth = FirebaseAuth.getInstance();
         mDatabase = FirebaseDatabase.getInstance().getReference();
-        mDatabase.child("cursos").setValue("All")
-                .addOnSuccessListener(new OnSuccessListener<Void>() {
-                    @Override
-                    public void onSuccess(Void aVoid) {
-                        Log.d("infoApp", "guardado exitosamente");
-                    }
-                })
-                .addOnFailureListener(new OnFailureListener() {
-                    @Override
-                    public void onFailure(@NonNull Exception e) {
-                        Log.e("infoApp", "onFailure", e.getCause());
-                    }
-                });
+
+
+        FirebaseUser user = mAuth.getCurrentUser();
+
+        TextView textView = findViewById(R.id.idNombre);
+        nombre = user.getDisplayName();
+        textView.setText(nombre);
+
+
 
     }
 }
