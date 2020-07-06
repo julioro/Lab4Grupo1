1d224ef671f1e323de149eb5be34bfc92fbb54a5
Eduardo <eduardo.requena@pucp.pe>
2020-07-05 21:23:06 -0500

    Firebase integrated

--- app/src/main/java/com/pucp/lab4egb/MainActivity.java
+++ app/src/main/java/com/pucp/lab4egb/MainActivity.java
@@ -1,17 +1,39 @@
 package com.pucp.lab4egb;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
-
+    DatabaseReference mDatabase;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+        mDatabase.child("cursos").setValue("All")
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        Log.d("infoApp", "guardado exitosamente");
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.e("infoApp", "onFailure", e.getCause());
+                    }
+                });
 
     }
 }
